808f166246eb227f700cf9a038284197a8d4424f
kszeto1 <kenny.szeto1@gmail.com>
2024-03-28 16:04:22 -0700

    Clean up boilerplate files and code, add About section

--- main.js
+++ main.js
@@ -1,24 +1,43 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
+  <nav>
+    <h1>Kenny Szeto</h1>
+    <ul>
+      <li><a href="./index.html">About</a></li>
+      <li>Projects</li>
+      <li>Resume</li>
+    </ul>
+  </nav>
+  <article>
+    <p>
+      Hey, I'm Kenny. I'm a software engineer based in the San Francisco Bay Area.
     </p>
-  </div>
-`
+    <p>
+      (1) As a software engineer with a master's degree in taxation, I combine technical skills and domain knowledge to create user-friendly and data-driven applications. I have experience working with technologies such as React, Node/Express, PostgreSQL, MongoDB, MySQL, and Amazon Web Services (AWS).
+    </p>
+    <p>
+      (2) Most recently, I developed and maintained intuitive user interfaces for CloudVirga’s proprietary loan origination software using Angular and React Native frameworks. I implemented interactive mobile-friendly forms, architected reusable UI components, and conducted UI testing across browsers and devices.
+    </p>
+    <p>
+    I am passionate about learning new technologies and solving complex problems. I am looking for opportunities to leverage my skills and expertise in frontend development to create innovative and impactful solutions.
+    </p>
 
-setupCounter(document.querySelector('#counter'))
+
+  </article>
+
+  <ul>
+    <li>
+      LinkedIn: <a href="https://www.linkedin.com/in/kenny-szeto/" target="_blank">@kenny-szeto</a>
+    </li>
+    <li>
+      GitHub: <a href="https://github.com/kszeto1" target="_blank">@kszeto1</a>
+    </li>
+    <li>
+      Email: <a href="mailto:kenny.szeto1@gmail.com" target="_blank">kenny.szeto1@gmail.com</a>
+    </li>
+  </ul>
+
+  </div>
+`;
